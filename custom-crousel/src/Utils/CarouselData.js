fdc2efc089b1e2e283e2d28810f8857f6ccb39d0
Abhishek Das <abhishekdas@nference.net>
2021-08-22 13:28:47 +0530

    added extra image carousel
    Signed-off-by: Abhishek Das <abhishekdas@nference.net>

--- custom-crousel/src/Utils/CarouselData.js
+++ custom-crousel/src/Utils/CarouselData.js
@@ -1,6 +1,7 @@
 import waterfall from "./../Images/waterfall.jpg"
 import forest from "./../Images/forest.jpg"
 import sunRise from "./../Images/sunRise.jpg"
+import ocean from "./../Images/ocean.jpg"
 export const images=[
     {
         title:"Water Fall",
@@ -14,5 +15,9 @@ export const images=[
         title:"Sun Rise",
         subtitle:"Sunrise (or sunup) is the moment when the upper rim of the Sun appears on the horizon in the morning. The term can also refer to the entire process of the solar disk crossing the horizon and its accompanying atmospheric effects.",
         img: sunRise
+    },{
+        title:"Ocean",
+        subtitle:"The ocean (also the sea or the world ocean) is the body of salt water which covers approximately 71% of the surface of the Earth and contains 97% of Earth's water",
+        img:ocean
     }
 ]
